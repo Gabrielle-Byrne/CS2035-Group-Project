fd6c702774007db5a20305cf018fe236632b5b22
Gabrielle-Byrne <156119029+Gabrielle-Byrne@users.noreply.github.com>
2026-03-16 15:36:23 -0300

    Camera Zoom

--- main.gd
+++ main.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 
 var can_change = false
-@onready var cameraB = $Player_B/Camera_B
-@onready var cameraA = $Player_A/Camera_A
+#@onready var cameraB = $Player_B/Camera_B
+#@onready var cameraA = $Player_A/Camera_A
+@onready var camera = $Camera2D
 
 
 enum {Player_A, Player_B}
@@ -25,14 +26,14 @@ func _process(delta):
 				print(state)
 				print("A active")
 				reset_char_switch_delay()
-				cameraA.make_current()
+				#cameraA.make_current()
 		Player_B:
 			if Input.is_action_just_pressed("switch_character") and can_change == true:
 				state = Player_A
 				print(state)
 				print("B active")
 				reset_char_switch_delay()
-				cameraB.make_current()
+				#cameraB.make_current()
 
 
 func reset_char_switch_delay():
