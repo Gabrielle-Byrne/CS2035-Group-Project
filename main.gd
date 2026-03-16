5f8a96972a5a2c60e31b35c77947a2e042a44018
Gabrielle-Byrne <156119029+Gabrielle-Byrne@users.noreply.github.com>
2026-03-16 14:56:18 -0300

    Co-op

--- main.gd
+++ main.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-var max_time = 1.0
-var time_left = 0.0
+
 var can_change = false
 @onready var cameraB = $Player_B/Camera_B
 @onready var cameraA = $Player_A/Camera_A
@@ -12,18 +11,12 @@ var state
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	time_left = max_time
 	state = Player_A
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if time_left > 0.0:
-		time_left -= delta
-		print(time_left)
-	elif time_left <= 0.0 and can_change == false:
-		print("Can now change characters")
-		can_change = true
+	can_change = true
 	
 	match state:
 		Player_A:
@@ -43,5 +36,4 @@ func _process(delta):
 
 
 func reset_char_switch_delay():
-	time_left = 0
 	can_change = false
--- player_a.gd
+++ player_a.gd
@@ -5,7 +5,7 @@ const SPEED = 200
 const JUMP_VELOCITY = -400.0
 
 func _ready():
-	$AnimatedSprite2D.play("B")
+	$AnimatedSprite2D.play("A")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,23 +13,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("b_jump") and is_on_floor():
+	if Input.is_action_just_pressed("a_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("b_left", "b_right")
+	var direction := Input.get_axis("a_left", "a_right")
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.play("B")
+		$AnimatedSprite2D.play("A")
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		$AnimatedSprite2D.stop()
 		
-	if(Input.is_action_just_pressed("b_left")):
+	if(Input.is_action_just_pressed("a_left")):
 		$AnimatedSprite2D.flip_h = false
-	if(Input.is_action_just_pressed("b_right")):
+	if(Input.is_action_just_pressed("a_right")):
 		$AnimatedSprite2D.flip_h = true
 		
 
