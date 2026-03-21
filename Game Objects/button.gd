266ae01ea7fab79c1e218924ca45d72e4de87bda
Ben Howe <ben.howe@unb.ca>
2026-03-21 14:52:13 -0300

    Fix buttons pressing on loading

--- Game Objects/button.gd
+++ Game Objects/button.gd
@@ -13,17 +13,19 @@ func _ready() -> void:
 	$StaticBody2D/CollisionShape2D.position.y = 2
 
 func _on_button_top_body_entered(body: Node2D) -> void:
-	pressed = true
-	$AnimatedSprite2D.play("pressed")
-	$StaticBody2D/CollisionShape2D.shape.size.y = 10
-	$StaticBody2D/CollisionShape2D.position.y = 4
-	button_pressed.emit()
+	if(body.is_in_group("Player")):
+		pressed = true
+		$AnimatedSprite2D.play("pressed")
+		$StaticBody2D/CollisionShape2D.shape.size.y = 10
+		$StaticBody2D/CollisionShape2D.position.y = 4
+		button_pressed.emit()
+		
 
 
 func _on_button_top_body_exited(body: Node2D) -> void:
-	pressed = false
-	$AnimatedSprite2D.play("default")
-	$StaticBody2D/CollisionShape2D.shape.size.y = 14
-	$StaticBody2D/CollisionShape2D.position.y = 2
-	button_unpressed.emit()
-	
+	if(body.is_in_group("Player")):
+		pressed = false
+		$AnimatedSprite2D.play("default")
+		$StaticBody2D/CollisionShape2D.shape.size.y = 14
+		$StaticBody2D/CollisionShape2D.position.y = 2
+		button_unpressed.emit()
