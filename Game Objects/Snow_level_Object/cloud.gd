845b2b6eec2adbcb9250f3ceeeefd8ac71a26091
Ben Howe <ben.howe@unb.ca>
2026-04-04 14:01:31 -0300

    Add moving cloud

--- Game Objects/Snow_level_Object/cloud.gd
+++ Game Objects/Snow_level_Object/cloud.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const SPEED = 1
+
 @export var player: Node2D
 
 @onready var rays = $WindAreas.get_children()
@@ -16,3 +18,5 @@ func _physics_process(delta: float) -> void:
 		var collider = ray.get_collider()
 		if(collider and collider.is_in_group("Player")):
 			collider.wind_speed += 100
+
+	position.y = move_toward(position.y, player.position.y, SPEED)
