66c0acd16aa581ed249e777194d64688478d6538
Hy Pham <giahy.baohan@gmail.com>
2026-04-04 19:55:32 -0300

    Add codes and 2 Area2d to carryable block so that both players can now push it. Add code to generate a raycast cone for hungry boi.

--- Game Objects/hungry.gd
+++ Game Objects/hungry.gd
@@ -13,8 +13,8 @@ var direction: Vector2
 var left_wander_bounds: Vector2
 var right_wander_bounds: Vector2
 
-var angle_cone_of_vision := deg_to_rad(30.0)
-var max_view_distance:= 100
+var angle_cone_of_vision := deg_to_rad(45.0)
+var max_view_distance:= 200
 var angle_between_rays := deg_to_rad(5.0)
 
 enum States{
@@ -28,13 +28,25 @@ var current_state = States.WANDER
 func _ready():
 	left_wander_bounds = self.position + Vector2(-100, 0)
 	right_wander_bounds = self.position + Vector2(0, 100)
+	generate_raycast()
 
 func _physics_process(delta: float) -> void:
 	handle_gravity(delta)
 	handle_movement(delta)
-	generate_raycast()
 	change_direction()
 	look_for_food()
+	
+	for ray in get_children():
+		if ray.is_in_group("vision"):
+			if ray.is_colliding():
+				var collider = ray.get_collider()
+				if collider == foodToFollow:
+					print("Found food!")
+					chase_food()
+				elif current_state == States.CHASE:
+					stop_chase()
+			elif current_state == States.CHASE:
+				stop_chase()
 	
 	if current_state == States.WANDER or current_state == States.CHASE:
 		sprite.play("walking")
@@ -48,8 +60,15 @@ func generate_raycast() -> void:
 		ray.target_position = Vector2.LEFT.rotated(angle) * max_view_distance
 		add_child(ray)
 		ray.enabled = true
+		ray.add_to_group("vision")
+
+func flip_ray() -> void:
+	for ray in get_children():
+		if ray.is_in_group("vision"):
+			ray.target_position = -ray.target_position
 
 func look_for_food():
+	#for ray_cast in get_children():
 	if ray_cast.is_colliding():
 		var collider = ray_cast.get_collider()
 		if collider == foodToFollow:
@@ -88,6 +107,7 @@ func change_direction() -> void:
 			else:
 				sprite.flip_h = true
 				ray_cast.target_position = Vector2(100, 0)
+				#flip_ray()
 	elif current_state == States.CHASE:
 		#Switch to following food object. Set direction to where food is
 		direction = (foodToFollow.position - self.position).normalized()
@@ -99,6 +119,7 @@ func change_direction() -> void:
 			#flip to right
 			sprite.flip_h = true
 			ray_cast.target_position = Vector2(100, 0)
+			#flip_ray()
 		else:
 			#flip to left
 			sprite.flip_h = false
