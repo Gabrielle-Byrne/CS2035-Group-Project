ab38027734dfb6a57a27d258cdb33fd15688f598
Hy Pham <giahy.baohan@gmail.com>
2026-04-05 15:43:43 -0300

    Add code to change direction upon seeing ledge.

--- Game Objects/hungry.gd
+++ Game Objects/hungry.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 @export var foodToFollow: CharacterBody2D
-@export var SPEED: int = 50
+@export var SPEED: int = 30
 @export var CHASE_SPEED: int = 100
-@export var ACCELERATION: int = 100
+@export var ACCELERATION: int = 200
+@export var tile: TileMapLayer
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @onready var rays: Array[RayCast2D] = [$AnimatedSprite2D/ray1, $AnimatedSprite2D/ray2, $AnimatedSprite2D/ray3,
 $AnimatedSprite2D/ray4, $AnimatedSprite2D/ray5, $AnimatedSprite2D/ray6, $AnimatedSprite2D/ray7]
+@onready var ray_down: RayCast2D = $AnimatedSprite2D/ray_down
+@onready var wall_detect: RayCast2D = $AnimatedSprite2D/wall_detect
 
 @onready var timer: Timer = $Timer
 
@@ -28,10 +31,13 @@ enum States{
 var current_state = States.IDLE
 
 func _ready():
-	left_wander_bounds = self.position + Vector2(-100, 0)
-	right_wander_bounds = self.position + Vector2(0, 100)
+	left_wander_bounds = self.position + Vector2(-150, 0)
+	print(left_wander_bounds)
+	right_wander_bounds = self.position + Vector2(150, 0)
+	print(right_wander_bounds)
 	currentScaleX = currentScaleX * self.scale.x
 	currentScaleY = currentScaleY * self.scale.x
+
 
 func _physics_process(delta: float) -> void:
 	handle_gravity(delta)
@@ -72,6 +78,63 @@ func handle_movement(delta: float) -> void:
 	move_and_slide()
 
 func change_direction() -> void:
+	#Look for ledges
+	if is_on_floor() and not ray_down.is_colliding():
+		
+		#Temporarily disable ray looking for food
+		for ray in rays:
+			ray.enabled = false
+			
+		#Turn around if there is no ledge.
+		if direction.x == 1:
+			direction = Vector2(-1, 0)
+			sprite.scale.x = currentScaleX
+		elif direction.x == -1:
+			direction = Vector2(1, 0)
+			sprite.scale.x = -currentScaleX
+		
+		if current_state == States.CHASE:
+			current_state = States.WANDER
+		
+		#Once turned around, re-enable the rays
+		for ray in rays:
+			ray.enabled = true
+		
+		return #Break out of function to prevent chasing or wander
+	
+	#if wall_detect.is_colliding():
+	#	var collider = wall_detect.get_collider()
+	#	print(collider.name)
+	#	if collider != foodToFollow:
+	#		var collision_point = tile.local_to_map(wall_detect.get_collision_point())
+	#		var tileAtlas = tile.get_cell_atlas_coords(collision_point)
+			
+	#		var notAWall: Array = [
+	#			Vector2i(0, 5),
+	#			Vector2i(1, 5),
+	#			Vector2i(2, 5),
+	#			Vector2i(3, 5),
+	#			Vector2i(4, 5),
+	#			Vector2i(5, 5),
+	#			Vector2i(6, 5),
+	#			Vector2i(7, 5),
+	#			Vector2i(12, 5),
+	#		]
+			
+	#		if tileAtlas in notAWall:
+	#			return
+			#Turn around if there is a wall
+	#		if direction.x == 1:
+	#			direction = Vector2(-1, 0)
+	#			sprite.scale.x = currentScaleX
+	#		elif direction.x == -1:
+	#			direction = Vector2(1, 0)
+	#			sprite.scale.x = -currentScaleX
+			
+	#		if current_state == States.CHASE:
+	#			current_state = States.WANDER
+	#		return
+	
 	if current_state == States.WANDER:
 		if direction.x == 1:
 			#move to the right
