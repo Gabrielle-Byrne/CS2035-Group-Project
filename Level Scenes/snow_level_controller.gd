dc33f7b5c914457f8141a0fa639a75182d02a574
Ben Howe <r8q69@gc112m14.cs.unb.ca>
2026-04-07 18:13:30 -0300

    fix cameras on rest of levels

--- Level Scenes/snow_level_controller.gd
+++ Level Scenes/snow_level_controller.gd
@@ -1,12 +1,7 @@
 extends Node2D
 
-var wind_scene = load("res://Game Objects/wind.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("reset"):
+		#get_tree().change_scene_to_file("res://Level Scenes/level_3.tscn")
+		get_tree().reload_current_scene()
