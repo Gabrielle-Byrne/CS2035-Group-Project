925772d615dee375b92308e6150a5dc6f2cefa8a
Ben Howe <r8q69@gc112m14.cs.unb.ca>
2026-04-07 19:04:20 -0300

    level switching

--- Level Scenes/main.gd
+++ Level Scenes/main.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+var currCam = 0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):	
 	if Input.is_action_just_pressed("reset"):
 		#get_tree().change_scene_to_file("res://Level Scenes/level_3.tscn")
 		get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("change_camera"):
+		if(currCam == 0):
+			currCam = 1
+		elif(currCam == 1):
+			currCam = 2
+		else:
+			currCam = 0
