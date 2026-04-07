9feb9890693a086b106039a467d2256db93a826a
Ben Howe <r8q69@gc112m14.cs.unb.ca>
2026-04-07 18:54:09 -0300

    Connect levels

--- Game Objects/stranded.gd
+++ Game Objects/stranded.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 
 var colors = ["Blue", "Orange", "Pink","Yellow"]
-var levels = ["res://Level Scenes/level_1.tscn", "res://Level Scenes/level_2.tscn", "res://Level Scenes/level_3.tscn"]
-var level_names = ["Level_1", "Level_2", "Level_3"]
+var levels = ["res://Level Scenes/level_1.tscn", "res://Level Scenes/level_2.tscn", "res://Level Scenes/level_3.tscn", "res://Level Scenes/food_level.tscn", "res://Level Scenes/food_level2.tscn", "res://Level Scenes/snow_level.tscn", "res://Level Scenes/final_level.tscn"]
+var level_names = ["Level_1", "Level_2", "Level_3", "Food_level", "Food_level2", "Level3", "final_level"]
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -38,5 +38,5 @@ func handleCollisions():
 				var next_level = levels[i + 1]
 				get_tree().change_scene_to_file(next_level)
 			else:
-				if !(get_tree().current_scene.name=="res://Level Scenes/end.tscn"):
-					get_tree().change_scene_to_file("res://Level Scenes/end.tscn")
+				if !(get_tree().current_scene.name=="res://Level Scenes/end_screen.tscn"):
+					get_tree().change_scene_to_file("res://Level Scenes/end_screen.tscn")
